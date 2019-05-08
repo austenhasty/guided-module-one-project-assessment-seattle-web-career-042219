cde9d5d1bcc3590d3b731f9508dd3e283cd45763
austenhasty <hastyae@gmail.com>
2019-05-08 14:40:19 -0700

    corrected has_many through bugs in user/ restaurant classes and greated CLI folder and greet method

--- lib/models/restaurant.rb
+++ lib/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ActiveRecord::Base
   has_many :visits
-  has_many :users through :visits
+  has_many :users, through: :visits
   has_and_belongs_to_many :categories
   has_many :restaurant_categories
 end
